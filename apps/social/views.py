b7fa86b7b64d78160d45a8fd474f7cc99a57bdca
Ilhomjon <rahimovilhomjon25@gmail.com>
2026-08-17 16:47:22 +0500

    FollowAPIView view created

---
+++ apps/social/views.py
@@ -0,0 +1,56 @@
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+
+from notification.models import Notification, FOLLOW, UN_FOLLOW
+from .models import Follow
+from .serializers import FollowSerializer
+
+
+class FollowAPIView(generics.CreateAPIView):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        follower = request.user
+        following = serializer.validated_data['following']
+
+        follow = Follow.objects.filter(
+            follower=follower,
+            following=following
+        ).first()
+
+        if follow:
+            follow.delete()
+
+            Notification.objects.create(
+                recipient=following,
+                author=follower,
+                notification_type=UN_FOLLOW,
+            )
+
+            return Response({
+                'success': True,
+                'message': 'Unfollow successful.',
+                'is_following': False,
+            })
+
+        Follow.objects.create(
+            follower=follower,
+            following=following,
+        )
+
+        Notification.objects.create(
+            recipient=following,
+            author=follower,
+            notification_type=FOLLOW,
+        )
+
+        return Response({
+            'success': True,
+            'message': 'Follow successful.',
+            'is_following': True,
+        }, status=status.HTTP_201_CREATED)
