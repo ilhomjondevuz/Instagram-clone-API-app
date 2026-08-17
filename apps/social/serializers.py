feee751c142f62eb741d609fa6fb7826a9332069
Ilhomjon <rahimovilhomjon25@gmail.com>
2026-08-18 01:06:49 +0500

    FollowSerializer added following and follower owners

--- apps/social/serializers.py
+++ apps/social/serializers.py
@@ -1,18 +1,23 @@
 from rest_framework import serializers
 
+from apps.accounts.serializers import UserSerializer
 from .models import Follow
 
 
 class FollowSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
     follower = serializers.PrimaryKeyRelatedField(read_only=True)
+    following_owner = serializers.SerializerMethodField('get_following_owner')
+    follower_owner = serializers.SerializerMethodField('get_follower_owner')
 
     class Meta:
         model = Follow
         fields = (
             'id',
-            'follower',
-            'following',
+            'follower',  # qaysi foydalanuvchi follow(kuzatmoqda) bosmoqda
+            'following',  # qaysi foydalanuvchi kuzatilmoqda
+            'following_owner',  # kim follow bosganligi
+            'follower_owner',  # kimga follow bosganligi
             'created_at',
         )
         read_only_fields = (
@@ -20,3 +25,9 @@ class FollowSerializer(serializers.ModelSerializer):
             'follower',
             'created_at',
         )
+
+    def get_following_owner(self, instance) -> UserSerializer:
+        return UserSerializer(instance.follower).data
+
+    def get_follower_owner(self, instance) -> UserSerializer:
+        return UserSerializer(instance.following).data
