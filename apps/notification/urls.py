3661c82592881465bd9dfb6a5237c03099b07e18
Ilhomjon <rahimovilhomjon25@gmail.com>
2026-08-18 14:50:08 +0500

    my unread notifications added

--- apps/notification/urls.py
+++ apps/notification/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import MyListNotificationAPIView, MyListReadNotificationAPIView
+from .views import MyListNotificationAPIView, MyListReadNotificationAPIView, MyListUnReadNotificationAPIView
 
 urlpatterns = [
     path('my-list/', MyListNotificationAPIView.as_view(), name='my-list'),
     path('my-list/read/', MyListReadNotificationAPIView.as_view(), name='my-list-read'),
+    path('my-list/unread/', MyListUnReadNotificationAPIView.as_view(), name='my-list-unread'),
 ]
